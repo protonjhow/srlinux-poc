f1fb5e4309ba28b9573515057fe9ef5525012208
John Howard <john.howard@proton.ch>
2023-03-10 09:51:41 +0100

    new linux container for bonding

--- configs/pod1-cab3-srv1.sh
+++ configs/pod1-cab3-srv1.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-cat > /etc/network/interfaces << EOF
-auto eth1
-iface eth1 inet static
-  # 10.{pod}.{leaf}.{nextfree}/30
-  #address 10.1.1.10
-  #netmask 255.255.255.254
-  address 192.168.0.3
-  netmask 255.255.255.248
-#iface eth1 inet6 static
-  # fd00:{pod}{leaf}::{port}:{vrf}:1/64
-  #address fd00:11::3:100:2
-  #netmask 64
-  #pre-up echo 0 > /proc/sys/net/ipv6/conf/eth1/accept_ra
+# NEW WAY
+# setup untagged on team0
+sudo ip address add 192.168.0.3/28 dev team0
+sudo ip link set team0 up
 
-auto eth2
-iface eth2 inet static
-  # 10.{pod}.{leaf}.{nextfree}/30
-  #address 10.1.2.10
-  #netmask 255.255.255.254
-  address 192.168.0.11
-  netmask 255.255.255.248
-#iface eth2 inet6 static
-  # fd00:{pod}{leaf}::{port}:{vrf}:1/64
-  #address fd00:12::3:100:2
-  #netmask 64
-  #pre-up echo 0 > /proc/sys/net/ipv6/conf/eth1/accept_ra
-EOF
+# setup a vlan on team0
+sudo vconfig add team0 200
+sudo ifconfig team0.200 10.200.200.33 netmask 255.255.255.0
+sudo ip link set team0.200 up
 
-ifup eth1
-ifup eth2
+# Add route pointing to vl200 agw on the leaf switch
+sudo ip route add 10.0.0.0/8 via 10.200.200.254 dev team0.200
+
+# OLD WAY
+# cat > /etc/network/interfaces << EOF
+# auto eth1
+# iface eth1 inet static
+#   # 10.{pod}.{leaf}.{nextfree}/30
+#   #address 10.1.1.10
+#   #netmask 255.255.255.254
+#   address 192.168.0.3
+#   netmask 255.255.255.248
+# #iface eth1 inet6 static
+#   # fd00:{pod}{leaf}::{port}:{vrf}:1/64
+#   #address fd00:11::3:100:2
+#   #netmask 64
+#   #pre-up echo 0 > /proc/sys/net/ipv6/conf/eth1/accept_ra
+
+# auto eth2
+# iface eth2 inet static
+#   # 10.{pod}.{leaf}.{nextfree}/30
+#   #address 10.1.2.10
+#   #netmask 255.255.255.254
+#   address 192.168.0.11
+#   netmask 255.255.255.248
+# #iface eth2 inet6 static
+#   # fd00:{pod}{leaf}::{port}:{vrf}:1/64
+#   #address fd00:12::3:100:2
+#   #netmask 64
+#   #pre-up echo 0 > /proc/sys/net/ipv6/conf/eth1/accept_ra
+# EOF
+
+# ifup eth1
+# ifup eth2
