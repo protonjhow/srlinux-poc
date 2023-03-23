f085e1216a767c23b50940ea604229c5ae5107fc
John Howard <john.howard@proton.ch>
2023-03-23 14:08:02 +0100

    black test_fabric_interfaces

--- test_fabric_interfaces.py
+++ test_fabric_interfaces.py
@@ -5,7 +5,7 @@ import json
 DEVICE_LIST = [
     "clab-zur1-pods-pod1-sp1",
     "clab-zur1-pods-pod1-sp2",
-    "clab-zur1-pods-pod1-sp3"
+    "clab-zur1-pods-pod1-sp3",
 ]
 INTERFACE_LIST = [
     "ethernet-1/1",
@@ -20,46 +20,47 @@ jsonrpc_path = "/jsonrpc"
 default_cred = ("admin", "NokiaSrl1!")
 headers = {"Content-type": "application/json"}
 
+
 def build_rpc_request(path: str, datastore: str) -> str:
     body = {
         "jsonrpc": "2.0",
         "id": 0,
         "method": "get",
-        "params": {
-            "commands": [{"path": path, "datastore": datastore}]
-        },
+        "params": {"commands": [{"path": path, "datastore": datastore}]},
     }
     return body
 
 
 def assert_interface_status(response: requests.models.Response) -> tuple:
     interface_state = response.json()["result"][0]
-    if interface_state['admin-state'] == "enable":
-        if interface_state['oper-state'] == "up":
+    if interface_state["admin-state"] == "enable":
+        if interface_state["oper-state"] == "up":
             state_str = "up/up"
             state_bool = True
-        else: 
+        else:
             state_str = "up/down"
             state_bool = False
-    else: 
+    else:
         state_str = "down"
         state_bool = False
     return (state_bool, state_str)
-    
+
 
 for device in DEVICE_LIST:
     url = f"https://{device}{jsonrpc_path}"
     for interface in INTERFACE_LIST:
         response = requests.post(
             url,
-            data=json.dumps(build_rpc_request(f"/interface[name={interface}]", "state")),
+            data=json.dumps(
+                build_rpc_request(f"/interface[name={interface}]", "state")
+            ),
             headers=headers,
             auth=requests.auth.HTTPBasicAuth(*default_cred),
             verify="clab-zur1-pods/.tls/ca/ca.pem",
         )
-        success,result = assert_interface_status(response)
+        success, result = assert_interface_status(response)
 
         if success:
             print(f"\U00002705 {device}: {interface}: {result}")
-        else: 
+        else:
             print(f"\U0000274C {device}: {interface}: {result}")
