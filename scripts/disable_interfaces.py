7fd0e1396eed3bee8c160112101c1ba1b6b2392f
John Howard <john.howard@proton.ch>
2023-04-03 22:06:15 +0200

    run enable/disable only on cores

--- scripts/disable_interfaces.py
+++ scripts/disable_interfaces.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 
 # Modules
-import datetime
-import logging
 from nornir.init_nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_pygnmi.tasks import gnmi_get, gnmi_set
@@ -24,23 +22,20 @@ CONFIG_MSG = [
 
 # Body
 if __name__ == "__main__":
-    # Get initial timestamp
-    start_time = datetime.datetime.now()
-    print(f"Execution started at {start_time}")
-
     # Initialise Nornir
-    nrn = InitNornir(config_file=NORNIR_CONFIG)
+    nr = InitNornir(config_file=NORNIR_CONFIG)
 
-    # Perform action
-    # result1 = nrn.run(task=gnmi_capabilities)
-    # print_result(result1)
-    result2 = nrn.run(task=gnmi_get, encoding="json_ietf", path=PATH)
-    print_result(result2)
-    result3 = nrn.run(task=gnmi_set, encoding="json_ietf", update=CONFIG_MSG)
-    print_result(result3, severity_level=logging.DEBUG)
-    result4 = nrn.run(task=gnmi_get, encoding="json_ietf", path=PATH)
-    print_result(result4)
-
-    # Get final timestamp
-    end_time = datetime.datetime.now()
-    print(f"Execution took {end_time - start_time} and completed at {end_time}")
+    # filter to just Cores 
+    cores = nr.inventory.groups['cores']
+    
+    # Check before
+    interfaces_state_before = cores.run(task=gnmi_get, encoding="json_ietf", path=PATH)
+    print_result(interfaces_state_before)
+    
+    # change it
+    interfaces_state_change = cores.run(task=gnmi_set, encoding="json_ietf", update=CONFIG_MSG)
+    print_result(interfaces_state_change)
+    
+    # check it
+    interfaces_state_after = cores.run(task=gnmi_get, encoding="json_ietf", path=PATH)
+    print_result(interfaces_state_after)
