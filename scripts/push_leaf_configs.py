81d0891784cafa7795e546cc077f83f7a3a8cf9d
John Howard <john.howard@proton.ch>
2023-04-04 15:38:07 +0200

    label task runs

--- scripts/push_leaf_configs.py
+++ scripts/push_leaf_configs.py
@@ -30,11 +30,11 @@ def read_yaml_config(yaml_file):
     return config_msg
 
 
-def send_config_to_group(config_name, nornir_group):
+def send_config_to_group(run_name, config_name, nornir_group):
     config_msg = read_yaml_config(config_name)
     # change it
     update_request = nornir_group.run(
-        task=gnmi_set, encoding="json_ietf", update=config_msg
+        name=run_name, task=gnmi_set, encoding="json_ietf", update=config_msg
     )
     print_result(update_request)
 
@@ -60,19 +60,19 @@ if __name__ == "__main__":
     leaf6 = nr.filter(name__endswith="lf6")
 
     # order of things
-    send_config_to_group("pod1-lf1-interfaces", leaf1)
-    send_config_to_group("pod1-lf2-interfaces", leaf2)
-    send_config_to_group("pod1-rack1-interfaces", rack1)
-    send_config_to_group("pod1-interfaces", leaves)
-    send_config_to_group("pod1-tunnel-interfaces", leaves)
-    send_config_to_group("pod1-routing-policy-prefix-sets", leaves)
-    send_config_to_group("pod1-routing-policy-underlay", leaves)
-    send_config_to_group("pod1-lf1-network-instance-vrf-default", leaf1)
-    send_config_to_group("pod1-lf2-network-instance-vrf-default", leaf2)
-    send_config_to_group("pod1-lf1-network-instance-vrf-routing", leaf1)
-    send_config_to_group("pod1-lf2-network-instance-vrf-routing", leaf2)
-    send_config_to_group("pod1-rack1-system-network-instance", rack1)
-    send_config_to_group("pod1-network-instance-vrf-mgmt-infra", leaves)
-    send_config_to_group("pod1-routing-policy-vrf-leaks", leaves)
-    send_config_to_group("pod1-network-instance-vrf-frontend", leaves)
-    send_config_to_group("pod1-network-instance-vrf-backend", leaves)
+    send_config_to_group("Configure interfaces - LF1", "pod1-lf1-interfaces", leaf1)
+    send_config_to_group("Configure interfaces - LF2", "pod1-lf2-interfaces", leaf2)
+    send_config_to_group("Configure interfaces - Rack1", "pod1-rack1-interfaces", rack1)
+    send_config_to_group("Configure interfaces - Pod1", "pod1-interfaces", leaves)
+    send_config_to_group("Configure tunnel interfaces - Pod1", "pod1-tunnel-interfaces", leaves)
+    send_config_to_group("Configure routing policy prefixes - Pod1", "pod1-routing-policy-prefix-sets", leaves)
+    send_config_to_group("Configure routing policy underlay - Pod1", "pod1-routing-policy-underlay", leaves)
+    send_config_to_group("Configure VRF default - LF1", "pod1-lf1-network-instance-vrf-default", leaf1)
+    send_config_to_group("Configure VRF default - LF2", "pod1-lf2-network-instance-vrf-default", leaf2)
+    send_config_to_group("Configure VRF Routing - LF1", "pod1-lf1-network-instance-vrf-routing", leaf1)
+    send_config_to_group("Configure VRF Routing - LF2", "pod1-lf2-network-instance-vrf-routing", leaf2)
+    send_config_to_group("Configure network-instance system - Pod1", "pod1-rack1-system-network-instance", rack1)
+    send_config_to_group("Configure VRF MGMT_Infra - Pod1", "pod1-network-instance-vrf-mgmt-infra", leaves)
+    send_config_to_group("Configure routing policy VRF Leaking - Pod1", "pod1-routing-policy-vrf-leaks", leaves)
+    send_config_to_group("Configure VRF Frontend - Pod1", "pod1-network-instance-vrf-frontend", leaves)
+    send_config_to_group("Configure VRF Backend - Pod1", "pod1-network-instance-vrf-backend", leaves)
