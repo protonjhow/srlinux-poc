e9aaa682f99face28272f241133f33f74d959d2c
John Howard <john.howard@proton.ch>
2023-04-04 09:31:08 +0200

    black cleanup

--- scripts/enable_interfaces.py
+++ scripts/enable_interfaces.py
@@ -26,17 +26,19 @@ if __name__ == "__main__":
     # Initialise Nornir
     nr = InitNornir(config_file=NORNIR_CONFIG)
 
-    # filter to just Cores 
+    # filter to just Cores
     cores = nr.filter(F(groups__contains="core"))
-    
+
     # Check before
     interfaces_state_before = cores.run(task=gnmi_get, encoding="json_ietf", path=PATH)
     print_result(interfaces_state_before)
-    
+
     # change it
-    interfaces_state_change = cores.run(task=gnmi_set, encoding="json_ietf", update=CONFIG_MSG)
+    interfaces_state_change = cores.run(
+        task=gnmi_set, encoding="json_ietf", update=CONFIG_MSG
+    )
     print_result(interfaces_state_change)
-    
+
     # check it
     interfaces_state_after = cores.run(task=gnmi_get, encoding="json_ietf", path=PATH)
     print_result(interfaces_state_after)
--- scripts/push_leaf_interfaces.py
+++ scripts/push_leaf_interfaces.py
@@ -9,8 +9,8 @@ from nornir_pygnmi.tasks import gnmi_get, gnmi_set
 from ruamel.yaml import YAML
 from rich import print
 
-yaml = YAML(typ='safe')
-with open('configs/pod1-interfaces.yml', 'r') as file:
+yaml = YAML(typ="safe")
+with open("configs/pod1-interfaces.yml", "r") as file:
     content = yaml.load(file)
 
 # Statics
@@ -29,17 +29,19 @@ if __name__ == "__main__":
     # Initialise Nornir
     nr = InitNornir(config_file=NORNIR_CONFIG)
 
-    # filter to just leaves 
+    # filter to just leaves
     leaves = nr.filter(F(groups__any=POD1_RACKS))
-    
+
     # Check before
     # interfaces_state_before = leaves.run(task=gnmi_get, encoding="json_ietf", path=PATH)
     # print_result(interfaces_state_before)
-    
+
     # change it
-    interfaces_state_change = leaves.run(task=gnmi_set, encoding="json_ietf", update=CONFIG_MSG)
+    interfaces_state_change = leaves.run(
+        task=gnmi_set, encoding="json_ietf", update=CONFIG_MSG
+    )
     print_result(interfaces_state_change)
-    
+
     # check it
     # interfaces_state_after = leaves.run(task=gnmi_get, encoding="json_ietf", path=PATH)
     # print_result(interfaces_state_after)
